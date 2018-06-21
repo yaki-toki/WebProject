58b3b87d4246acf898e2016d494e24739949dade
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-06-21 13:36:49 +0900

    Model추가

--- FinalyTest/src/user/Car.java
+++ FinalyTest/src/user/Car.java
@@ -1,30 +1,21 @@
 package user;
 
 public class Car {
-	private String number;
-	private int idx;
-	private String userID;
 	private String modelID;
+	private String manufacturer;
+	private String modelName;
+	private int modelYear;
+	private int modelDsp;
+	private String modelType;
+	private String modelEngine;
+	private String modelGear;
+	private int gearNumber;
+	private int modelMax;
+	private String modelDriving;
+	private String modelFuel;
 	
-	public String getNumber() {
-		return number;
-	}
-	public void setNumber(String number) {
-		this.number = number;
-	}
-	
-	public int getIdx() {
-		return idx;
-	}
-	public void setIdx(int idx) {
-		this.idx = idx;
-	}
-	
-	public String getUserID() {
-		return userID;
-	}
-	public void setUserID(String userID) {
-		this.userID = userID;
+	public Car() {
+		modelID=null;
 	}
 	
 	public String getModelID() {
@@ -33,4 +24,71 @@ public class Car {
 	public void setModelID(String modelID) {
 		this.modelID = modelID;
 	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public String getModelName() {
+		return modelName;
+	}
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+	public int getModelYear() {
+		return modelYear;
+	}
+	public void setModelYear(int modelYear) {
+		this.modelYear = modelYear;
+	}
+	public int getModelDsp() {
+		return modelDsp;
+	}
+	public void setModelDsp(int modelDsp) {
+		this.modelDsp = modelDsp;
+	}
+	public String getModelType() {
+		return modelType;
+	}
+	public void setModelType(String modelType) {
+		this.modelType = modelType;
+	}
+	public String getModelEngine() {
+		return modelEngine;
+	}
+	public void setModelEngine(String modelEngine) {
+		this.modelEngine = modelEngine;
+	}
+	public String getModelGear() {
+		return modelGear;
+	}
+	public void setModelGear(String modelGear) {
+		this.modelGear = modelGear;
+	}
+	public int getGearNumber() {
+		return gearNumber;
+	}
+	public void setGearNumber(int gearNumber) {
+		this.gearNumber = gearNumber;
+	}
+	public int getModelMax() {
+		return modelMax;
+	}
+	public void setModelMax(int modelMax) {
+		this.modelMax = modelMax;
+	}
+	public String getModelDriving() {
+		return modelDriving;
+	}
+	public void setModelDriving(String modelDriving) {
+		this.modelDriving = modelDriving;
+	}
+	public String getModelFuel() {
+		return modelFuel;
+	}
+	public void setModelFuel(String modelFuel) {
+		this.modelFuel = modelFuel;
+	}
+	
 }
