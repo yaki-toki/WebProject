45a07c803f75f6892094079900c66fbbd451e200
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-10-12 15:44:12 +0900

    Get방식의 애노테이션 매핑

---
+++ SecondSemester_2018/src/ServletBasic/HelloServlet.java
@@ -0,0 +1,29 @@
+package ServletBasic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("euc-kr");
+		response.setContentType("text/html; charset=euc-kr");
+
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>인사</title></head>");
+		out.println("<body>");
+		out.println("안녕하세요, ");
+		out.println(request.getParameter("name"));
+		out.println("님");
+		out.println("</body></html>");
+	}
+}
