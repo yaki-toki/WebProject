a435e7e4fb23c745fc4b7f489d7f90b1772db265
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-10-26 14:54:45 +0900

    View의 Controller구현

--- SecondSemester_2018/src/MVC/ControllerUsingFile.java
+++ SecondSemester_2018/src/MVC/ControllerUsingFile.java
@@ -1,11 +1,85 @@
 package MVC;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Properties;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 public class ControllerUsingFile extends HttpServlet {
 
 	private Map<String, CommandHandler> commandHandlerMap = new HashMap<>();
+	
+	public void init() throws ServletException{
+		String configFile = getInitParameter("configFile");
+		// getInitParameter이라는 메소드는 HttpServlet에서 만들어져 있는 메소드
+		// 상속받은 클래스는 자식이 마음대로 사용 가능
+		// configFile이라는 값은 프로퍼티에서 만들 값
+		
+		Properties prop = new Properties();
+		String configFilePath = getServletContext().getRealPath(configFile);
+		try(FileReader fis = new FileReader(configFilePath)) {
+			prop.load(fis);
+		}catch(IOException e) {
+			throw new ServletException(e);
+		}
+		
+		Iterator keyIter = prop.keySet().iterator();
+		
+		while(keyIter.hasNext()) {
+			String command = (String) keyIter.next();
+			String handlerClassName = prop.getProperty(command);
+			try {
+				Class<?> handlerClass = Class.forName(handlerClassName);
+				// 어떤 클래스인지 모르기 때문에 ?로 선언 한다.
+				// ?로 선언을 하면 일단 모든 클래스를 사용할 수 있도록 해준다.
+				
+				CommandHandler handlerInstance = (CommandHandler) handlerClass.newInstance();
+				// Interface는 new로 선언할 수 없다.
+				// handlerClass.newInterface()는 CommandHandler인터페이스의 인스턴스를 선언한다.
+				
+				commandHandlerMap.put(command, handlerInstance);
+				
+			}catch(ClassNotFoundException | InstantiationException
+					| IllegalAccessException e) {
+				throw new ServletException(e);
+			}
+			
+		}
+	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+	
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String command = request.getParameter("cmd");
+		CommandHandler handler = commandHandlerMap.get(command);
+		if(handler == null) {
+			handler = new NullHandler();
+			// 404에러 응담
+		}
+		String viewPage = null;
+		try {
+			viewPage = handler.process(request, response);
+		} catch (Throwable e) {
+			throw new ServletException(e);
+		}
+		if(viewPage != null) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+			dispatcher.forward(request, response);
+		}
+	}
 }
