a7c576f344d0ee7d8eadceb48416aa8333706746
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-11-09 15:01:00 +0900

    CommandHandler라는 인터페이스 구현

--- SecondSemester_2018/src/command/CommandHandler.java
+++ SecondSemester_2018/src/command/CommandHandler.java
@@ -3,6 +3,6 @@ package command;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface CommandHadler {
+public interface CommandHandler {
 	public ActionForward process(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
