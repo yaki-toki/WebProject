13f0fb350cbf807a2b85375f16dd8d02f9446266
kimjiha <rlawl@DESKTOP-KOHV3DK>
2019-05-15 16:13:07 +0900

    Kakao Model

---
+++ Example_01/src/main/java/com/spring/example/kakao/KakaoDTO.java
@@ -0,0 +1,54 @@
+package com.spring.example.kakao;
+
+public class KakaoDTO {
+	private String id;
+	private String nickname;
+	private String profile_image;
+	private String thumbnail_image;
+	private String email;
+	private String status;
+	private String created;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getProfile_image() {
+		return profile_image;
+	}
+	public void setProfile_image(String profile_image) {
+		this.profile_image = profile_image;
+	}
+	public String getThumbnail_image() {
+		return thumbnail_image;
+	}
+	public void setThumbnail_image(String thumbnail_image) {
+		this.thumbnail_image = thumbnail_image;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+}
