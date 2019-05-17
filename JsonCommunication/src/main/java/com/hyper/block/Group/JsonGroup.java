8d5d83545c5dcdc3142ba8113364f28bb94e4512
kimjiha <rlawl@DESKTOP-KOHV3DK>
2019-05-17 16:17:14 +0900

    Group Interface

---
+++ JsonCommunication/src/main/java/com/hyper/block/Group/JsonGroup.java
@@ -0,0 +1,17 @@
+package com.hyper.block.Group;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+public interface JsonGroup{
+	public final String ipAddr = "http://210.123.254.231:3000/api/";
+	public GroupModel getParser(JsonObject object) throws Exception;
+	public JsonArray GetGroup() throws MalformedURLException, ProtocolException, UnsupportedEncodingException, IOException, Exception;
+	public JsonObject GetGroupById(String email) throws MalformedURLException, ProtocolException, UnsupportedEncodingException, IOException, Exception;
+	public String dataPost(GroupModel model) throws MalformedURLException, ProtocolException, UnsupportedEncodingException, IOException, Exception;
+}
