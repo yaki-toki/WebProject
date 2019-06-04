1bc81dd85bfed90f9270edec6791ed21b84eca24
kimjiha <rlawl@DESKTOP-KOHV3DK>
2019-06-04 18:00:18 +0900

    quotation replace 추가

--- JsonCommunication/src/main/java/com/hyper/block/util/ResourceParser.java
+++ JsonCommunication/src/main/java/com/hyper/block/util/ResourceParser.java
@@ -1,10 +1,16 @@
 package com.hyper.block.util;
 
 public class ResourceParser {
+	
 	public String nameParser(String name) {
 		
 		name = name.substring(name.lastIndexOf("#") + 1);
 		
 		return name;
+	}
+	
+	public String quotationParser(String script) {
+		script = script.replace("\"", "");
+		return script;
 	}
 }
